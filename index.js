a3e573fe89cd93498da604d8905cad3ffd7db81a
Steven Meads <sbmeads@gmail.com>
2017-11-07 15:33:57 -0800

    add production env asset logic

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// How assets will be served in production
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve production assets
+  app.use(express.static('client/build'));
+  // Express will serve the index.html file if it doesn't recongize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is litening on port ${PORT}`);
 });
