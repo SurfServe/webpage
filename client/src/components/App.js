fe88d113cca5840f622d4e763b10a6c0f8433384
Steven Meads <sbmeads@gmail.com>
2017-11-08 13:02:14 -0800

    add components folder

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
