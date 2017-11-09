420f0f5e50944f63f4fda6d963d77827732cefb7
Steven Meads <sbmeads@gmail.com>
2017-11-09 10:01:55 -0800

    add Mission component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Header from './Header';
+import Mission from './Mission';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Mission />
       </div>
     );
   }
