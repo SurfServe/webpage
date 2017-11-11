9f84aa2cb95311ccba7865e8204b06de4fcc0a7f
Steven Meads <sbmeads@gmail.com>
2017-11-10 21:04:13 -0800

    add more context

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -9,8 +9,12 @@ const Projects = () => {
                 <p className="long-copy">Check out our most recent projects</p>
             </div>
             <div className="row">
-                <div className="col span-1-of-1 box">
-                    <h3>Mexico</h3>
+                <div className="col span-1-of-2 box">
+                    <h3>Ninos De La Promesa</h3>
+                    <p>Ninos de la Promesa rescues children from physical, mental, and sexual abuse in the homes and streets of Tijuana. They serve particularly children found in the red light district of downtown Tijuana. They have have a loving and warm home for 50 children who receive all they need to live and go to school all the way through university. They also help them to help others.</p>
+                </div>
+                <div className="col span-1-of-2 box">
+                    <h3>Addaptive Surf Camp</h3>
                 </div>
             </div>
         </section>
