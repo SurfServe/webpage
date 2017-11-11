73724da3c687b06f92f5adb5dd66b4ac65bae068
Steven Meads <sbmeads@gmail.com>
2017-11-11 07:22:56 -0800

    add PartnerHeading component

---
+++ client/src/components/PartnerHeading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/PartnerHeading.css';
+
+const PartnerHeading = () => {
+    return (
+        <section className="partner-heading-section">
+            <div className="row">
+                <h2>Partner</h2>
+                <p className='long-copy'>
+                    Lorem ipsum dolor sit amet, vivamus imperdiet eu, duis lobortis.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default PartnerHeading;
