6be1b4173d3db39c2461973c473329e35ea0075f
Steven Meads <sbmeads@gmail.com>
2018-01-14 16:54:16 -0700

    add image to partner heading

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,75 +1,90 @@
-import React, { Component } from 'react';
-import '../styles/ContactForm.css';
-
+import React, { Component } from "react";
+import "../styles/ContactForm.css";
 
 class ContactForm extends Component {
-    render() {
-        return(
-            <div className="form-box">
-                <div className="row">
-                    <h2>We'd love to hear from you</h2>
-                </div>
-                <div className="row">
-                    <form className="contact-form" action="#" method="post">
-                        <div className="row">
-                            <div className="col span-1-of-3">
-                                <label htmlFor="name">Name</label>
-                            </div>
-                            <div className="col span-2-of-3">
-                                <input type="text" name="name" id="name" placeholder="Your name" required />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col span-1-of-3">
-                                <label htmlFor="email">Email</label>
-                            </div>
-                            <div className="col span-2-of-3">
-                                <input type="email" name="email" id="email" placeholder="Your email" required />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col span-1-of-3">
-                                <label htmlFor="find-us">How did you find us?</label>
-                            </div>
-                            <div className="col span-2-of-3">
-                                <select name="find-us" id="find-us" defaultValue="Friends">
-                                    <option value="friends">Friends</option>
-                                    <option value="search">Search engine</option>
-                                    <option value="facebook">Facebook</option>
-                                    <option value="instagram">Instagram</option>
-                                    <option value="other">Other</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col span-1-of-3">
-                                <label htmlFor="news">Newsletter</label>
-                            </div>
-                            <div className="col span-2-of-3">
-                                <input type="checkbox" name="news" id="news" checked /> Yes, please
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col span-1-of-3">
-                                <label htmlFor="message">Drop us a message</label>
-                            </div>
-                            <div className="col span-2-of-3">
-                                <textarea name="message" placeholder="Your message"></textarea>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col span-1-of-3">
-                                <label>&nbsp;</label>
-                            </div>
-                            <div className="col span-2-of-3">
-                                <input type="submit" value="Send it!"/>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="form-box section-contact-form">
+        <div className="row">
+          <h2>We'd love to hear from you</h2>
+        </div>
+        <p className="long-copy">
+        To learn more about volunteering, donating, or if you know of an organization in need of our help, please fill out this form.
+        </p>
+        <div className="row">
+          <form className="contact-form" action="#" method="post">
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="col span-2-of-3">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your name"
+                  required
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="col span-2-of-3">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your email"
+                  required
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label htmlFor="find-us">How did you find us?</label>
+              </div>
+              <div className="col span-2-of-3">
+                <select name="find-us" id="find-us" defaultValue="Friends">
+                  <option value="friends">Friends</option>
+                  <option value="search">Search engine</option>
+                  <option value="facebook">Facebook</option>
+                  <option value="instagram">Instagram</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label htmlFor="news">Newsletter</label>
+              </div>
+              <div className="col span-2-of-3">
+                <input type="checkbox" name="news" id="news" checked /> Yes,
+                please
+              </div>
+            </div>
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label htmlFor="message">Drop us a message</label>
+              </div>
+              <div className="col span-2-of-3">
+                <textarea name="message" placeholder="Your message" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col span-1-of-3">
+                <label>&nbsp;</label>
+              </div>
+              <div className="col span-2-of-3">
+                <input type="submit" value="Send it!" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContactForm;
--- client/src/components/PartnerHeading.js
+++ client/src/components/PartnerHeading.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import '../styles/PartnerHeading.css';
+import React from "react";
+import "../styles/PartnerHeading.css";
+import MissionData from "../data/mission";
 
 const PartnerHeading = () => {
+  const missionStatements = MissionData.map(statement => {
     return (
-        <section className="partner-heading-section">
-            <div className="row">
-                <h2>Partner</h2>
-                <p className='long-copy'>
-                    Lorem ipsum dolor sit amet, vivamus imperdiet eu, duis lobortis.
-                </p>
-            </div>
-        </section>
+      <div key={statement.header} className="col span-1-of-3 box">
+        <i className={statement.icon} />
+        <h3>{statement.header}</h3>
+        <p>{statement.paragraph}</p>
+      </div>
     );
+  });
+  return (
+    <section className="partner-heading-section">
+      <div className="row">
+        <h2>Partner</h2>
+        <p className="long-copy">
+          Lorem ipsum dolor sit amet, vivamus imperdiet eu, duis lobortis.
+        </p>
+        <div className="row">{missionStatements}</div>
+        <div className="dontate-contact-box" >
+          <a className="btn btn-full" href="#">Donate</a>
+          <a className="btn btn-ghost" href="#">Contact</a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PartnerHeading;
