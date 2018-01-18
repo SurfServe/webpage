2bdccca29d209663df5f2ea8021121a3de6b94ab
Steven Meads <sbmeads@gmail.com>
2018-01-17 22:56:15 -0700

    implement formik with ContactForm component

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,97 +1,119 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
 import { withFormik } from 'formik';
 import "../styles/ContactForm.css";
 
-class ContactForm extends Component {
-  render() {
-    return (
-      <div className="form-box section-contact-form">
-        <div className="row">
-          <h2>We'd love to hear from you</h2>
-        </div>
-        <p className="long-copy">
-        To learn more about volunteering, donating, or if you know of an organization in need of our help, please fill out this form.
-        </p>
-        <div className="row">
-          <form className="contact-form" action="#" method="post">
-            <div className="row">
-              <div className="col span-1-of-3">
-                <label htmlFor="name">Name</label>
-              </div>
-              <div className="col span-2-of-3">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Your name"
-                  required
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col span-1-of-3">
-                <label htmlFor="email">Email</label>
-              </div>
-              <div className="col span-2-of-3">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Your email"
-                  required
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col span-1-of-3">
-                <label htmlFor="find-us">How did you find us?</label>
-              </div>
-              <div className="col span-2-of-3">
-                <select name="find-us" id="find-us" defaultValue="Friends">
-                  <option value="friends">Friends</option>
-                  <option value="search">Search engine</option>
-                  <option value="facebook">Facebook</option>
-                  <option value="instagram">Instagram</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col span-1-of-3">
-                <label htmlFor="news">Newsletter</label>
-              </div>
-              <div className="col span-2-of-3">
-                <input type="checkbox" name="news" id="news" checked /> Yes,
-                please
-              </div>
-            </div>
-            <div className="row">
-              <div className="col span-1-of-3">
-                <label htmlFor="message">Drop us a message</label>
-              </div>
-              <div className="col span-2-of-3">
-                <textarea name="message" placeholder="Your message" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col span-1-of-3">
-                <label>&nbsp;</label>
-              </div>
-              <div className="col span-2-of-3">
-                <input type="submit" value="Send it!" />
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
+// ENUMS
+import { FIND_US_OPTIONS } from '../components/enums';
 
-export default  withFormik({
-  name: '',
-  email: '',
-  findUs: '',
-  newsletter: false,
-  message: '',
+const ContactForm = ({ values, handleChange, handleSubmit }) => {
+  console.log('VALUES', values);
+  return (
+    <div className="form-box section-contact-form">
+      <div className="row">
+        <h2>We'd love to hear from you</h2>
+      </div>
+      <p className="long-copy">
+      To learn more about volunteering, donating, or if you know of an organization in need of our help, please fill out this form.
+      </p>
+      <div className="row">
+        <form className="contact-form" action="#" method="post">
+          <div className="row">
+            <div className="col span-1-of-3">
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="col span-2-of-3">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Your name"
+                required
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col span-1-of-3">
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="col span-2-of-3">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Your email"
+                required
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col span-1-of-3">
+              <label htmlFor="find-us">How did you find us?</label>
+            </div>
+            <div className="col span-2-of-3">
+              <select
+                name="findUs"
+                id="find-us"
+                defaultValue="Select one"
+                onChange={handleChange}
+              >
+                {FIND_US_OPTIONS.map(opt => (<option value={opt.value}>{opt.label}</option>))};
+              </select>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col span-1-of-3">
+              <label htmlFor="news">Newsletter</label>
+            </div>
+            <div className="col span-2-of-3">
+              <input
+                type="checkbox"
+                name="newsletter"
+                id="news"
+                checked={values.newsletter}
+                onChange={handleChange}
+              />
+              Yes, please
+            </div>
+          </div>
+          <div className="row">
+            <div className="col span-1-of-3">
+              <label htmlFor="message">Drop us a message</label>
+            </div>
+            <div className="col span-2-of-3">
+              <textarea
+                name="message"
+                placeholder="Your message"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col span-1-of-3">
+              <label>&nbsp;</label>
+            </div>
+            <div className="col span-2-of-3">
+              <input type="submit" value="Send it!" onSubmit={handleSubmit} />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default withFormik({
+  mapPropsToValues: () => ({
+    name: '',
+    email: '',
+    findUs: '',
+    newsletter: true,
+    message: '',
+  }),
+
+  handleSubmit: values => {
+    console.log('Values', values);
+  },
 })(ContactForm);
---
+++ client/src/components/enums.js
@@ -0,0 +1,8 @@
+export const FIND_US_OPTIONS = [
+  { value: "select one", label: "Select one" },
+  { value: "friends", label: "Friends" },
+  { value: "search", label: "Search" },
+  { value: "facebook", label: "Facebook" },
+  { value: "instagram", label: "Instagram" },
+  { value: "other", label: "Other" },
+];
