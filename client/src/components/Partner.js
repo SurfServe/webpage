61c1bddfacf5398164232c57f73dbcbbaf373a75
Steven Meads <sbmeads@gmail.com>
2018-01-18 22:04:48 -0700

    add location photos

--- client/src/components/Partner.js
+++ client/src/components/Partner.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import '../styles/Partner.css';
-import Slider from 'react-slick';
-import PatagoniaLogo from '../assets/img/patagonia.png';
-import StanceLogo from '../assets/img/stance.png';
-import DoyleLogo from '../assets/img/doyle.png';
+import React, { Component } from "react";
+import "../styles/Partner.css";
+import Slider from "react-slick";
+import PatagoniaLogo from "../assets/img/patagonia.png";
+import StanceLogo from "../assets/img/stance.png";
+import DoyleLogo from "../assets/img/doyle.png";
 
 class Partner extends Component {
-    render() {
-        const settings = {
-            dots: false,
-            arrows: false,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            fade: true,
-            cssEase: 'linear',
-            autoplay: true,
-            autoplaySpeed: 3000,
-            pauseOnHover: false
-        }
-        return (
-            <section className="partner-section">
-                <div className="row">
-                    <Slider {...settings}>
-                        <div>
-                            <img src={PatagoniaLogo} alt="Patagonia logo" />
-                        </div>
-                        <div>
-                            <img src={DoyleLogo} alt="Doyle logo"/>
-                        </div>
-                        <div>
-                            <img src={StanceLogo} alt="Stance logo" />
-                        </div>
-                    </Slider>
-                </div>
-            </section>
-        );
-    }
-};
+  render() {
+    const settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true,
+      cssEase: "linear",
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: false
+    };
+    return (
+      <section className="partner-section">
+        <div className="row">
+          <Slider {...settings}>
+            <div>
+              <img src={PatagoniaLogo} alt="Patagonia logo" />
+            </div>
+            <div>
+              <img src={DoyleLogo} alt="Doyle logo" />
+            </div>
+            <div>
+              <img src={StanceLogo} alt="Stance logo" />
+            </div>
+          </Slider>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Partner;
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "../styles/Projects.css";
 
+// IMAGES
+import NINOS_DE_LA_PROMES from '../assets/img/ninos-de-la-promesa.jpg';
+import ADDPTIVE_SURF from '../assets/img/addaptive-surf.jpg'
+
 const Projects = () => {
   return (
     <section className="projects-section">
@@ -13,26 +17,32 @@ const Projects = () => {
       <div className="row">
         <div className="col span-1-of-2 project-box">
           <h3>Ninos De La Promesa</h3>
-          <p>
+          <div className="location-photo">
+            <img src={NINOS_DE_LA_PROMES} alt="Ninos de la Promesa" />
+          </div>
+          {/* <p>
             Ninos de la Promesa rescues children from physical, mental, and
             sexual abuse in the homes and streets of Tijuana. They serve
             particularly children found in the red light district of downtown
             Tijuana. They have have a loving and warm home for 50 children who
             receive all they need to live and go to school all the way through
             university. They also help them to help others.
-          </p>
+          </p> */}
           <div className="gallery-btn">View Gallery</div>
         </div>
         <div className="col span-1-of-2 project-box">
           <h3>Addaptive Surf Camp</h3>
-          <p>
+          <div className="location-photo">
+            <img src={ADDPTIVE_SURF} alt="Addaptive surf" />
+          </div>
+          {/* <p>
             Ninos de la Promesa rescues children from physical, mental, and
             sexual abuse in the homes and streets of Tijuana. They serve
             particularly children found in the red light district of downtown
             Tijuana. They have have a loving and warm home for 50 children who
             receive all they need to live and go to school all the way through
             university. They also help them to help others.
-          </p>
+          </p> */}
           <div className="gallery-btn">View Gallery</div>
         </div>
       </div>
