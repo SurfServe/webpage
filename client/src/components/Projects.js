c0ac1ad1513defbde71fe14514fdbbaae1c3c9bd
Steven Meads <sbmeads@gmail.com>
2018-01-18 22:39:30 -0700

    format location-photo div

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -20,30 +20,20 @@ const Projects = () => {
           <div className="location-photo">
             <img src={NINOS_DE_LA_PROMES} alt="Ninos de la Promesa" />
           </div>
-          {/* <p>
+          <p>
             Ninos de la Promesa rescues children from physical, mental, and
-            sexual abuse in the homes and streets of Tijuana. They serve
-            particularly children found in the red light district of downtown
-            Tijuana. They have have a loving and warm home for 50 children who
-            receive all they need to live and go to school all the way through
-            university. They also help them to help others.
-          </p> */}
-          <div className="gallery-btn">View Gallery</div>
+            sexual abuse in the homes and streets of Tijuana.
+          </p>
         </div>
         <div className="col span-1-of-2 project-box">
           <h3>Addaptive Surf Camp</h3>
           <div className="location-photo">
             <img src={ADDPTIVE_SURF} alt="Addaptive surf" />
           </div>
-          {/* <p>
+          <p>
             Ninos de la Promesa rescues children from physical, mental, and
-            sexual abuse in the homes and streets of Tijuana. They serve
-            particularly children found in the red light district of downtown
-            Tijuana. They have have a loving and warm home for 50 children who
-            receive all they need to live and go to school all the way through
-            university. They also help them to help others.
-          </p> */}
-          <div className="gallery-btn">View Gallery</div>
+            sexual abuse in the homes and streets of Tijuana.
+          </p>
         </div>
       </div>
     </section>
